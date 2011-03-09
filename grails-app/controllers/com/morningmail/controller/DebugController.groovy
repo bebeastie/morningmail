38a64bfefa426ed574e3368662e6f9822f805b0d
Blake Barnes <blake.barnes@gmail.com>
2011-03-08 20:12:19 -0500

    added send email debug tool

--- grails-app/controllers/com/morningmail/controller/DebugController.groovy
+++ grails-app/controllers/com/morningmail/controller/DebugController.groovy
@@ -6,6 +6,17 @@ import javax.persistence.*;
 import org.springframework.orm.jpa.EntityManagerFactoryUtils;
 import com.morningmail.domain.*
 
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.AddressException;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+import javax.mail.internet.MimeMultipart;
+import javax.mail.internet.MimeBodyPart;
+import javax.mail.BodyPart;
+
 class DebugController {
 	def entityManagerFactory
 	EntityManager em
@@ -31,5 +42,51 @@ class DebugController {
 		Query q = em.createQuery("delete from TwitterLink t");
 		int number = q.executeUpdate();
 		render(view:'index', model:[returnValue:number])
+	}
+	
+	def sendEmail = {
+		if (!params.submit) {
+			render(view:'sendEmail', model:[returnValue:""])
+			return
+		}
+		
+		Properties props = new Properties();
+		Session session = Session.getDefaultInstance(props, null);
+			
+		String subject = params.subject
+		
+		try {
+			Message msg = new MimeMessage(session);
+
+			msg.setFrom(new InternetAddress(params.from, "MorningMail"));
+			msg.addRecipient(Message.RecipientType.TO,
+							new InternetAddress(params.to));
+			msg.setSubject(subject);
+		   
+			MimeMultipart mp = new MimeMultipart();
+			BodyPart tp = new MimeBodyPart();
+			tp.setText(params.plainText, "UTF-8");
+			mp.addBodyPart(tp);
+
+			tp = new MimeBodyPart();
+			tp.setContent(params.html
+				, "text/html");
+			mp.addBodyPart(tp);
+
+			mp.setSubType("alternative");
+
+			msg.setContent(mp);
+
+			Transport.send(msg);
+			
+			render(view:'sendEmail', model:[returnValue:"Success"])
+		} catch (AddressException e) {
+			log.error("Problems sending email", e)
+			render(view:'sendEmail', model:[returnValue:"Error: " + e])
+		} catch (MessagingException e) {
+			log.error("Problems sending email ", e)
+			render(view:'sendEmail', model:[returnValue:"Error: " + e])
+		}
+		
 	}
 }
